0208b16d7039892413fdef1ac3d8500ffa1f01a9
fli <francisatsnaptech@gmail.com>
2017-10-23 17:17:27 -0700

    Full Width Pages Fixed

--- page-templates/page-full-width.php
+++ page-templates/page-full-width.php
@@ -3,15 +3,11 @@
 Template Name: Full Width
 */
 get_header(); ?>
-
-<?php get_template_part( 'template-parts/featured-image' ); ?>
-
-<div class="main-wrap full-width">
-	<main class="main-content">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
-			<?php comments_template(); ?>
-		<?php endwhile;?>
-	</main>
-</div>
+    <div id="page-full-width" class="container-viewport">
+        <?php //get_template_part('template-parts/featured-image'); ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <?php get_template_part('template-parts/content', 'page'); ?>
+                    <?php comments_template(); ?>
+                <?php endwhile; ?>
+    </div>
 <?php get_footer();
--- page.php
+++ page.php
@@ -11,16 +11,11 @@
  */
 
  get_header(); ?>
-
- <?php get_template_part( 'template-parts/featured-image' ); ?>
-
- <div class="main-wrap">
+ <?php //get_template_part( 'template-parts/featured-image' ); ?>
 	 <main class="main-content">
 		 <?php while ( have_posts() ) : the_post(); ?>
 		 	<?php get_template_part( 'template-parts/content', 'page' ); ?>
 			<?php comments_template(); ?>
 		 <?php endwhile;?>
 	 </main>
- <?php get_sidebar(); ?>
- </div>
  <?php get_footer();
